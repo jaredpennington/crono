78bd60c500325b544a3c2fee64026a5ca37ff7b0
jaredpennington <94023319+jaredpennington@users.noreply.github.com>
2022-04-24 17:22:24 -0400

    updates with friend :)

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -12,14 +12,22 @@ const userController = {
         })
     },
 
-    updateUser(req, res) {
-        User.updateOne(req.body)
-        //again idk what else i need to put here
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+        .then(dbUserData => {
+          if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id!' });
+            return;
+          }
+          res.json(dbUserData);
+        })
+        .catch(err => res.json(err));
     },
 
-    deleteUser(req, res) {
-        User.delete(req.body)
-        //idk what else to put since im deleting the data created
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.json(err));       
     }
     // All next functions go beneath here
 
--- models/Thought.js
+++ models/Thought.js
@@ -1,30 +1,53 @@
 const { Schema, model } = require('mongoose');
 
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+          },
+
+        reactionBody: {
+            type: String,
+            required: true,
+            minlength: 1, maxLength: 280
+        },
+
+        username: {
+            type: String,
+            required: true
+         },
+     
+        createdAt: {
+             type: Date,
+             defult: Date.now,
+             get: createdAtVal => date.format(createdAtVal, 'ddd, MMM DD YYYY')
+         },
+
+
+    }
+)
+
 const ThoughtSchema = new Schema({
     thoughtText: {
         type: String,
         required: true,
-        //what do i put for character count?
+        minlength: 1, maxLength: 280
     },
 
     username: {
        type: String,
        required: true
     },
-    
-    // is this right????? lmao 
-    //   reaction: [
-    //     {
-    //         type: Schema.Types.ObjectId,
-    //         ref: 'Thought'
-    //     }
-    // ],
 
     createdAt: {
         type: Date,
-        defult: Date.now
-        //getter method to format the timestamp on query
-    }
+        defult: Date.now,
+        get: createdAtVal => date.format(createdAtVal, 'ddd, MMM DD YYYY')
+    },
+
+    reaction: [ReactionSchema]
 },
 {
     toJSON: {
@@ -33,14 +56,18 @@ const ThoughtSchema = new Schema({
     },
     id: false
 });
+
+
+
 // idk if i did this right for the schema settings
 
 ThoughtSchema.virtual('reactionCount').get(function() {
     return this.reactions.length;
 })
 
+
 // create the Pizza model using the PizzaSchema
-const User = model('User', UserSchema);
+const Thought = model('Thought', ThoughtSchema);
 
 // export the Pizza model
-module.exports = User;
+module.exports = Thought;
