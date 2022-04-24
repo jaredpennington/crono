d9a8e6bc887cb2779bb5e5866dc0b8358c16a4bf
jaredpennington <94023319+jaredpennington@users.noreply.github.com>
2022-04-24 18:00:41 -0400

    more and more updates and fun

--- models/index.js
+++ models/index.js
@@ -1,6 +1,4 @@
 const User = require('./User');
 const Thought = require('./Thought');
-// import Thought
 
-// Export Thought as well
 module.exports = {User, Thought}
--- server.js
+++ server.js
@@ -21,3 +21,60 @@ mongoose.connect(
 mongoose.set("debug", true);
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+
+
+
+const {User, Thought} = require('./models');
+const { db } = require("./models/User");
+
+app.get('/api/users', (req, res) => {
+  User.find({})
+  .then(User => {
+    res.json(User);
+  })
+  .catch(err => {
+    res.json(err);
+  });
+});
+
+app.get('/api/users/:id', ({ params }, res) => {
+  User.findOne({ _id: params.id })
+    .populate({
+      path: 'thoughts',
+      select: '-__v'
+    })
+    .populate({
+      path: 'friends',
+      select: '-__v'
+    })
+    .select('-__v')
+    .then(UserData => res.json(UserData))
+    .catch(err => {
+      console.log(err);
+      res.sendStatus(400);
+    });
+})
+
+app.post('/api/users', ({ body }, res) => {
+    User.create(body)
+      .then(UserData => res.json(UserData))
+      .catch(err => res.json(err))
+});
+
+app.put('/api/users/:id', ({ params, body }, res) => {
+  User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+    .then(UserData => {
+      if (!UserData) {
+        res.status(404).json({ message: 'No user found with this id!' });
+        return;
+      }
+      res.json(UserData);
+    })
+    .catch(err => res.json(err));
+});
+
+app.delete('/api/users/:id', ({ params }, res) => {
+  User.findOneAndDelete({ _id: params.id })
+    .then(UserData => res.json(UserData))
+    .catch(err => res.json(err))
+});
